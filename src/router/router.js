c2cdcd57c2745b778a792586a108e3b2605605a9
Kovalenko Vasyl <kovalenkova26@gmail.com>
2022-06-22 18:02:09 +0300

    change router

--- src/router/router.js
+++ src/router/router.js
@@ -3,24 +3,22 @@ import PublicPage from "../pages/PublicPage";
 import Todo from "../pages/Todo";
 import LoginPage from "../pages/LoginPage"
 import RequireAuth from "../api/RequireAuth"
-import AuthStatus from "../api/AuthStatus";
 
 function Router () {
     return (
         <Routes>
                 <Route path="/" element={<PublicPage />} />
                 <Route path="/login" element={<LoginPage />} />
-                <Route element={<AuthStatus/>}>
-                    <Route
-                        path="/todo"
-                        element={
-                            <RequireAuth>
-                                <Todo />
-                            </RequireAuth>
-                        }
-                    />
-                </Route>
+                <Route
+                    path="/todo"
+                    element={
+                        <RequireAuth>
+                            <Todo />
+                        </RequireAuth>
+                    }
+                />
         </Routes>
+
     )
 }
 
