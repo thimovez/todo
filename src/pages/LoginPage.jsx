69cbf0a7e6b140e7d41beac8b58ac6b03b0c8ad2
Kovalenko Vasyl <kovalenkova26@gmail.com>
2022-07-06 10:02:37 +0300

    add login page and promo

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,35 +1,13 @@
-import {useLocation, useNavigate} from "react-router-dom";
-import useAuth from "../hooks/useAuth";
+import Form from "../components/Form/Form";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 
 function LoginPage() {
-    let navigate = useNavigate();
-    let auth = useAuth();
-
-    function handleSubmit(event) {
-        event.preventDefault();
-
-        let formData = new FormData(event.currentTarget);
-        let username = formData.get("username");
-
-        auth.signin(username, () => {
-            navigate("/todo", { replace: true });
-        });
-    }
-
     return (
         <div>
-            <p>You must log in to view the page at /todo</p>
-
-            <form onSubmit={handleSubmit}>
-                <label>
-                    Username: <input name="username" type="email" />
-                </label>{" "}
-                <label>
-                    Password: <input name="password" type="password" />
-                </label>{" "}
-
-                <button type="submit">Login</button>
-            </form>
+            <Header/>
+            <Form/>
+            <Footer/>
         </div>
     );
 }
